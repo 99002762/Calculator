fbd34dee755838197a47e6bf4939b8c34609637d
99002762 <72461476+99002762@users.noreply.github.com>
2020-10-09 11:45:07 +0530

    Update function.h

--- inc/function.h
+++ inc/function.h
@@ -3,16 +3,17 @@
 
 #include <stdio.h>
 
-#include<math.h>
-int add(int input1,int input2);
-int subtract(int input1,int input2);
-int multiply(int input1,int input2);
-int divide(int input1,int input2);
-double squareroot(double triginput);
-int modulus(int input1,int input2);
-int fact(int input1);
-double cosine(double triginput);
-double sine(double triginput);
-int power(int input1,int input2);
+#include<math.h>//includes math library files
+
+//function definitions present in calculator project
+
+int pro_add(int input1,int input2);
+int pro_subtract(int input1,int input2);
+int pro_multiply(int input1,int input2);
+int pro_divide(int input1,int input2);
+double pro_squareroot(double triginput);
+int pro_modulus(int input1,int input2);
+int pro_fact(int input1);
+int pro_power(int input1,int input2);
 
 #endif
